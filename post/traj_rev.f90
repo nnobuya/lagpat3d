57f22d42a644b413e167569b87c93bc7288e3daa
Nobuya NISHIMURA <nobuya@Nobuyas-MBP.home>
2016-03-23 01:25:24 +0000

    update

--- post/traj_rev.f90
+++ post/traj_rev.f90
@@ -2,17 +2,27 @@ program traj_rev
 
   implicit none
 
+  !..param.
   integer  , parameter:: ndim = 3
   character, parameter:: bin*11 = 'unformatted'
 
-  integer:: npt, idt_ini, idt_fin
-  integer, allocatable::
+  integer, parameter:: npt0  = 100
+  logical, parameter:: debug = .false.
 
-  integer:: idt, i, j, k
+  integer:: npt, idt_ini, idt_fin
+
+  integer, allocatable:: istg(:), ipt(:,:,:), ist_pt(:,:)
+  real(4), allocatable:: ti(:), dt(:), x_pt(:,:,:), v_pt(:,:,:), &
+      & d_pt(:,:), t_pt(:,:), ye_pt(:,:), en_pt(:,:)
+  real(4):: rad
+
+  integer:: idt, ier, i, j, k
   character:: op_file*100
 
 
+
   open(50,file = './res/part_set.log', action = 'read')
+
   read(50,*)
   read(50,*) npt, i, j, k
   close(50)
@@ -20,8 +30,18 @@ program traj_rev
   idt_ini = 1
   idt_fin = 162
 
+  allocate( istg(idt_ini:idt_fin), ti(idt_ini:idt_fin), dt(idt_ini:idt_fin), &
+       & ist_pt(1:npt,idt_ini:idt_fin), ipt(1:ndim,1:npt,idt_ini:idt_fin), &
+       & x_pt(1:ndim,1:npt,idt_ini:idt_fin), &
+       & v_pt(1:ndim,1:npt,idt_ini:idt_fin), d_pt(1:npt,idt_ini:idt_fin), &
+       & t_pt(1:npt,idt_ini:idt_fin), ye_pt(1:npt,idt_ini:idt_fin), &
+       & en_pt(1:npt,idt_ini:idt_fin), stat = ier)
+
+  write(*,'(5x, "- read data")')
 
   lp_read_hydr: do idt = idt_ini, idt_fin
+
+     if (mod(idt,50) == 0) write(*,*) idt
 
      write(op_file,'("./res/hydr/hydr_", i5.5, ".dat")') idt
 
@@ -29,13 +49,43 @@ program traj_rev
 
      read(50) istg(idt), ti(idt), dt(idt)
      read(50) ist_pt(1:npt,idt)
-     read(50) ipt(1:ndim,1:npt), x_pt(1:ndim,1:npt), v_pt(1:ndim,1:npt), &
-          & d_pt(1:npt), t_pt(1:npt), ye_pt(1:npt), en_pt(1:npt)
-
+     read(50) ipt(1:ndim,1:npt,idt), x_pt(1:ndim,1:npt,idt), &
+          & v_pt(1:ndim,1:npt,idt), d_pt(1:npt,idt), t_pt(1:npt,idt), &
+          & ye_pt(1:npt,idt), en_pt(1:npt,idt)
 
      close(50)
 
+
   end do lp_read_hydr
+
+
+
+  write(*,'(5x, "- write data")')
+
+  if (debug) npt = npt0
+
+  lp_write_data: do i = 1, npt
+
+     if (mod(i,5000) == 0) write(*,*) i, npt
+
+     write(op_file,'("./res/traj/traj_", i7.7, ".dat")') i
+     open(60,file = op_file, action = 'write')
+
+     write(60,'("#   Time", 11x, "Density", 8x, "T", 14x, &
+          & "Entropy", 8x, "Ye", 13x, "Radius     ")')
+
+     lp_pt_evol:do idt = idt_fin, idt_ini, -1
+        rad = sqrt(sum(x_pt(1:ndim,i,idt) *x_pt(1:ndim,i,idt)))
+        write(60,'(1p, *(e15.7))') &
+           & ti(idt) - ti(idt_fin), &
+           & d_pt(i,idt), t_pt(i,idt), en_pt(i,idt), ye_pt(i,idt), rad
+     end do lp_pt_evol
+
+     close(60)
+
+  end do lp_write_data
+
+
 
 
 
